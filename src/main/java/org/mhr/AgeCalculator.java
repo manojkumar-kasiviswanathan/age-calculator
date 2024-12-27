15fa2fd7c645aa9e280439f752a19432c48dea94
manojkumarkasiviswanathan <manojkumar.kasiviswanathan@gmail.com>
2024-12-27 10:49:19 +0000

    feat: feature to get user input

--- src/main/java/org/mhr/AgeCalculator.java
+++ src/main/java/org/mhr/AgeCalculator.java
@@ -1,8 +1,15 @@
 package org.mhr;
 
+import java.util.Scanner;
+
+
 public class AgeCalculator {
 
-    public String setUpCheck() {
-        return "Hello from the setup";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your birthday in the format YYYY/MM/DD: \n");
+        String dateOfBirth = scanner.nextLine(); //TODO in next implementation is to validate this input
+        scanner.close();
     }
 }
