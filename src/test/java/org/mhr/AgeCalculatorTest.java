ada50c93cb121783b41dfbcf392d8fa4434d9a60
manojkumarkasiviswanathan <manojkumar.kasiviswanathan@gmail.com>
2024-12-27 14:55:32 +0000

    refactor: optimized junit test for testGenerateBirthdayMessages

--- src/test/java/org/mhr/AgeCalculatorTest.java
+++ src/test/java/org/mhr/AgeCalculatorTest.java
@@ -1,12 +1,16 @@
 package org.mhr;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AgeCalculatorTest {
 
@@ -30,17 +34,20 @@ class AgeCalculatorTest {
     }
 
 
+    static Stream<Arguments> provideBirthdayMessages() {
+        return Stream.of(
+                org.junit.jupiter.params.provider.Arguments.of(LocalDate.now().plusDays(1), "You are not born yet, but you know how to program!"),
+                org.junit.jupiter.params.provider.Arguments.of(LocalDate.now(), "Are you sure you were born today?"),
+                org.junit.jupiter.params.provider.Arguments.of(LocalDate.now().minusYears(33), "Happy Birthday!\nYour age is: 33 years."),
+                org.junit.jupiter.params.provider.Arguments.of(LocalDate.now().minusYears(101).minusDays(1), "Amazing, you are 101 years old. That is really impressive!"),
+                org.junit.jupiter.params.provider.Arguments.of(LocalDate.now().minusYears(25).minusDays(1), "Your age is: 25 years.")
+        );
+    }
+
     @ParameterizedTest
-    @CsvSource({
-            "2024-12-28, 'You are not born yet, but you know how to program!'",
-            "2024-12-27, Are you sure you were born today?",
-            "1991-12-27, 'Happy Birthday!\nYour age is: 33 years.'",
-            "1923-01-01, 'Amazing, you are 101 years old. That is really impressive!'",
-            "2000-01-01, Your age is: 24 years."
-    })
-    void testGenerateBirthdayMessage(String dateOfBirth, String expectedMessage) {
-        LocalDate parseDateOfBirth = LocalDate.parse(dateOfBirth);
-        String actualMessage = AgeCalculator.generateBirthdayMessage(parseDateOfBirth);
+    @MethodSource("provideBirthdayMessages")
+    void testGenerateBirthdayMessages(LocalDate dateOfBirth, String expectedMessage) {
+        String actualMessage = AgeCalculator.generateBirthdayMessage(dateOfBirth);
         assertEquals(expectedMessage, actualMessage);
     }
 }
